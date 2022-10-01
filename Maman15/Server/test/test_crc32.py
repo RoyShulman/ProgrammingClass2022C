decce4ebbc67770ab6951ec3635979beae274c76
royshulman <roy.shulman@gmail.com>
2022-10-01 16:04:13 +0300

    server crc32

---
+++ Maman15/Server/test/test_crc32.py
@@ -0,0 +1,24 @@
+import unittest
+from server.crc32 import crc32
+
+
+class CRC32Test(unittest.TestCase):
+    def test_crc32(self):
+        tests = {
+            b"hello there": 3141126258,
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+            b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": 134456518
+        }
+
+        for data, checksum in tests.items():
+            with self.subTest(data=data, checksum=checksum):
+                self.assertEqual(crc32(data), checksum)
+
+
+if __name__ == "__main__":
+    unittest.main()
