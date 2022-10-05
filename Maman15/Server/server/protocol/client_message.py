4d6834ba63793b033147fa0bf224d204a4d26dc5
royshulman <roy.shulman@gmail.com>
2022-10-05 21:50:29 +0200

    all client messages until now

--- Maman15/Server/server/protocol/client_message.py
+++ Maman15/Server/server/protocol/client_message.py
@@ -2,6 +2,7 @@ import struct
 from enum import Enum
 from abc import ABC, abstractmethod
 from uuid import UUID
+from typing import Any
 
 
 class ClientMessageCode(Enum):
@@ -23,12 +24,15 @@ class AbstractClientMessageReader(ABC):
         raise NotImplementedError
 
 
+class ClientMessageCodeMissmatch(Exception):
+    pass
+
+
 class ClientMessage(ABC):
     """
     Base class for all client messages.
     The class should support unpacking from a AbstractClientMessageReader
     """
-
     @classmethod
     @abstractmethod
     def unpack(cls, reader: AbstractClientMessageReader) -> 'ClientMessage':
@@ -38,6 +42,14 @@ class ClientMessage(ABC):
     @abstractmethod
     def MESSAGE_FMT() -> str:
         raise NotImplementedError
+
+    @staticmethod
+    def read_fmt(fmt: str, reader: AbstractClientMessageReader) -> Any:
+        return struct.unpack(fmt, reader.read(struct.calcsize(fmt)))
+
+    def validate_message_code(self, expected: ClientMessageCode, real: ClientMessageCode):
+        if expected != real:
+            raise ClientMessageCodeMissmatch(expected, real)
 
 
 class ClientMessageHeader(ClientMessage):
@@ -49,8 +61,8 @@ class ClientMessageHeader(ClientMessage):
 
     @classmethod
     def unpack(cls, reader: AbstractClientMessageReader) -> 'ClientMessageHeader':
-        client_id, client_version, code, payload_size = struct.unpack(cls.MESSAGE_FMT(),
-                                                                      reader.read(struct.calcsize(cls.MESSAGE_FMT())))
+        client_id, client_version, code, payload_size = cls.read_fmt(cls.MESSAGE_FMT(),
+                                                                     reader)
         return cls(UUID(bytes=client_id), client_version, ClientMessageCode(code), payload_size)
 
     @staticmethod
@@ -64,3 +76,77 @@ class ClientMessageHeader(ClientMessage):
             self.client_version == other.client_version and \
             self.code == other.code and \
             self.payload_size == other.payload_size
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+
+class ClientRegistrationRequest(ClientMessage):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @classmethod
+    def unpack(cls, reader: AbstractClientMessageReader) -> 'ClientRegistrationRequest':
+        name = cls.read_fmt(cls.MESSAGE_FMT(), reader)[0]
+        return cls(name.decode())
+
+    @staticmethod
+    def MESSAGE_FMT() -> str:
+        return "<255s"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ClientRegistrationRequest):
+            return False
+        return self.name == other.name
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+
+class ClientPublicKeyMessage(ClientMessage):
+    def __init__(self, name: str, public_key: bytes) -> None:
+        self.name = name
+        self.public_key = public_key
+
+    @classmethod
+    def unpack(cls, reader: AbstractClientMessageReader) -> 'ClientPublicKeyMessage':
+        name, public_key = cls.read_fmt(cls.MESSAGE_FMT(), reader)
+        return cls(name.decode(), public_key)
+
+    @staticmethod
+    def MESSAGE_FMT() -> str:
+        return "<255s160s"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ClientPublicKeyMessage):
+            return False
+        return self.name == other.name and self.public_key == other.public_key
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+
+class UploadFileMessage(ClientMessage):
+    def __init__(self, client_uuid: UUID, filename: str, content: bytes) -> None:
+        self.client_uuid = client_uuid
+        self.filename = filename
+        self.content = content
+
+    @classmethod
+    def unpack(cls, reader: AbstractClientMessageReader) -> 'UploadFileMessage':
+        client_uuid, content_size, filename = cls.read_fmt(
+            cls.MESSAGE_FMT(), reader)
+        content = cls.read_fmt(f"<{content_size}s", reader)[0]
+        return cls(UUID(bytes=client_uuid), filename.decode(), content)
+
+    @staticmethod
+    def MESSAGE_FMT() -> str:
+        return "<16sI255s"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, UploadFileMessage):
+            return False
+        return self.client_uuid == other.client_uuid and self.filename == other.filename and self.content == other.content
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
--- Maman15/Server/test/test_client_message.py
+++ Maman15/Server/test/test_client_message.py
@@ -1,12 +1,16 @@
 import unittest
 import struct
+import secrets
 from unittest.mock import MagicMock
 from typing import cast
 from uuid import uuid4
 from server.protocol.client_message import (
     AbstractClientMessageReader,
     ClientMessageHeader,
-    ClientMessageCode
+    ClientMessageCode,
+    ClientRegistrationRequest,
+    ClientPublicKeyMessage,
+    UploadFileMessage
 )
 
 
@@ -16,23 +20,61 @@ class MockClientMessageReader(AbstractClientMessageReader):
 
 class ClientMessageTest(unittest.TestCase):
     def setUp(self) -> None:
+        self.client_uuid = uuid4()
         self.reader = MockClientMessageReader()
+        self.mock_reader = cast(MagicMock, self.reader.read)
 
     def tearDown(self) -> None:
         cast(MagicMock, self.reader.read).reset_mock()
 
     def test_unpack_header(self):
-        client_uuid = uuid4()
-        mock_reader = cast(MagicMock, self.reader.read)
-        mock_reader.return_value = struct.pack("<16sBHI", client_uuid.bytes,
-                                               3, ClientMessageCode.REGISTRATION.value, 8)
+        self.mock_reader.side_effect = [struct.pack("<16sBHI", self.client_uuid.bytes,
+                                                    3, ClientMessageCode.REGISTRATION.value, 8)]
 
         message = ClientMessageHeader.unpack(self.reader)
 
-        header = ClientMessageHeader(client_uuid, 3,
+        header = ClientMessageHeader(self.client_uuid, 3,
                                      ClientMessageCode.REGISTRATION, 8)
         self.assertEqual(header, message)
-        mock_reader.assert_called_once_with(struct.calcsize("<16sBHI"))
+        self.mock_reader.assert_called_once_with(struct.calcsize("<16sBHI"))
+
+    def test_unpack_client_registration(self):
+        name = struct.pack("<255s", b"coolname")
+        self.mock_reader.side_effect = [name]
+        message = ClientRegistrationRequest.unpack(self.reader)
+
+        expected_message = ClientRegistrationRequest(name.decode())
+        self.assertEqual(expected_message, message)
+        self.mock_reader.assert_called_once_with(struct.calcsize("<255s"))
+
+    def test_unpack_public_key(self):
+        public_key = secrets.token_bytes(160)
+        name = struct.pack("<255s", b"coolname")
+
+        self.mock_reader.side_effect = [
+            struct.pack("<255s160s", name, public_key)]
+        message = ClientPublicKeyMessage.unpack(self.reader)
+
+        expected_message = ClientPublicKeyMessage(name.decode(), public_key)
+        self.assertEqual(expected_message, message)
+        self.mock_reader.assert_called_once_with(struct.calcsize("<255s160s"))
+
+    def test_unpack_upload_file(self):
+        content_size = 5
+        filename = struct.pack("<255s", b"filename.jpeg")
+        content = b"12345"
+
+        self.mock_reader.side_effect = [
+            struct.pack("<16sI255s", self.client_uuid.bytes,
+                        content_size, filename),
+            struct.pack("<5s", content)]
+        message = UploadFileMessage.unpack(self.reader)
+
+        expected_message = UploadFileMessage(self.client_uuid,
+                                             filename.decode(), content)
+        self.assertEqual(expected_message, message)
+        self.assertEqual(2, self.mock_reader.call_count)
+        self.mock_reader.assert_called_with(struct.calcsize("<5s"))
 
 
 if __name__ == "__main__":
